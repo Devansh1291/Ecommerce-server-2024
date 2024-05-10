515fa5083f20c1ff2500a54f9584bf0ac0e7f278
DEVANSH TREHAN <devanshtrehan129@gmail.com>
2024-05-10 20:16:40 +0530

    Payment controller updated

--- src/controllers/payment.ts
+++ src/controllers/payment.ts
@@ -5,30 +5,20 @@ import ErrorHandler from "../utils/utility-class.js";
 
 
 export const createPaymentIntent = TryCatch(async (req, res, next) => {
-    const { amount, description,name,address:{state,city,country} } = req.body;
-
-    if (!amount) return next(new ErrorHandler("Please Enter the amount", 404));
-
+    const { amount } = req.body;
+  
+    if (!amount) return next(new ErrorHandler("Please enter amount", 400));
+  
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: Number(amount) * 100,
-        currency: "inr",
-        description: description || "Payment for goods/services", 
-        shipping:{
-            name:name,
-            address:{
-                state:state,
-                city:city,
-                country:country,   
-            }
-        }
+      amount: Number(amount) * 100,
+      currency: "inr",
     });
-
-    return res.status(200).json({
-        success: true,
-        message: "Payment Intent created",
-        clientSecret: paymentIntent.client_secret,
+  
+    return res.status(201).json({
+      success: true,
+      clientSecret: paymentIntent.client_secret,
     });
-});
+  });
 
 
 export const newCoupon=TryCatch(async(req,res,next)=>{
